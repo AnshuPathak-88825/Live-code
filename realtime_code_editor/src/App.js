9a822479fe3c916ea4b99c4f05fee36f0b1566e7
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-03-10 14:18:47 +0530

    Added home page UI with css

--- realtime_code_editor/src/App.js
+++ realtime_code_editor/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import { BrowserRouter, Route,Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import EditorPage from './pages/EditorPage';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>} ></Route>
-        <Route path='/Editor/:roomId' element={<EditorPage/>} ></Route>
+        <Routes>
+          <Route path='/' element={<Home />} ></Route>
+          <Route path='/Editor/:roomId' element={<EditorPage />} ></Route>
 
-      </Routes>
+        </Routes>
       </BrowserRouter>
 
     </div>
--- realtime_code_editor/src/pages/Home.js
+++ realtime_code_editor/src/pages/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="HomePageWrapper">
+            <div className="FormWrapper">
+                <img className="homePageLogo" src="/code-sync.png" alt="Code-sync-logo" />
+                <h4 className="Mainlabel">Paste invitation id</h4>
+                <div className="inputGroup">
+                    <input type='text' className="inputBox" placeholder="Room-Id"></input>
+                    <input type='text' className="inputBox" placeholder="User-Name"></input>
+                    <button className="btn joinBtn">Join</button>
+                    <span className="createInfo">
+                        If you don't have invite then create &nbsp;
+                        <a href="/" className="createNewBtn">create room</a>
+                    </span>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Home;
