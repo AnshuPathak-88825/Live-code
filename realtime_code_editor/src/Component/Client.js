27cedc232cab70559f30ffff37fa35019e72ddde
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-03-13 05:50:09 +0530

    created aside section of code editor

---
+++ realtime_code_editor/src/Component/Client.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Avatar from "react-avatar";
+const Client=({username})=>{
+    console.log(username);
+    return (
+        <div className="client">
+            <Avatar name={username} size={50} round="14px"  />
+            <span className="userName">{username}</span>
+        </div>
+    )
+}
+export default Client;
--- realtime_code_editor/src/pages/EditorPage.js
+++ realtime_code_editor/src/pages/EditorPage.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import Client from "../Component/Client";
+import Editor from "../Component/Editor";
 const EditorPage = () => {
-    return (
-        <div>EditorPage</div>
-    )
-}
+  const [clients, setclients] = useState([
+    { socketId: 1, username: "Rakesh k" },
+    { socketId: 2, username: "Aanshu" },
+  ]);
+  return (
+    <div className="minWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img
+              className="logoImage"
+              src="/code-sync.png"
+              alt="Code-sync-logo"
+            />
+            
+          </div>
+          <h3>Connected</h3>
+
+          <div className="clientList">
+              {clients.map((client) => (
+                <Client key={client.socketId} username={client.username} />
+              ))}
+            </div>
+        </div>
+        <button className="btn copyBtn">Copy Room ID</button>
+        <button className="btn leaveBtn">Leave</button>
+
+      </div>
+      <div className="editorwrap">
+        <Editor/>
+      </div>
+    </div>
+  );
+};
 export default EditorPage;
