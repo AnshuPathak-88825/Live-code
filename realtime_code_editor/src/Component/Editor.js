450129e68f411b55d6f2df204e36df4fd71c9d49
AnshuPathak-88825 <shubhanshupathak88825@gmail.com>
2023-03-15 21:59:55 +0530

    Editor design created

--- realtime_code_editor/src/Component/Editor.js
+++ realtime_code_editor/src/Component/Editor.js
@@ -1,7 +1,25 @@
-import React from "react"
-const Editor=()=>{
-    return (
-        <div>Editor</div>
-    )
-}
+import React, { useState } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { dracula, okaidia } from "@uiw/codemirror-theme-dracula";
+const Editor = () => {
+ const [code,setcode]=useState("console.log('hello world!');")
+
+  let change = (e) => {
+    
+  setcode(e);
+
+  };
+  console.log(code);
+  return (
+    
+    <CodeMirror
+      value={code}
+      style={{height:"100vh-20vh",fontSize:"16px",lineHeight:5,paddingTop:"20px"}}
+      extensions={[javascript({ jsx: true })]}
+      onChange={change}
+      theme={dracula}
+    />
+  );
+};
 export default Editor;
